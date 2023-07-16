989a05228589293b6d1fac60fbecb2492f30f9df
sukh <sukh@gmail.om.com>
2023-07-16 16:23:33 +0530

    commie

--- vars/owaspCheck.groovy
+++ vars/owaspCheck.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh "sudo /home/sukh/Downloads/dependency-check/bin/dependency-check.sh --project jenkins --scan var/lib/jenkins/workspace/pipeline-B/GthubActions --out jenkins.html"
+    sh "/home/sukh/Downloads/dependency-check/bin/dependency-check.sh --project jenkins --scan var/lib/jenkins/workspace/pipeline-B/GthubActions --out jenkins.html"
 }
