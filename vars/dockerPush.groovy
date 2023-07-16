590313101e85fb4dfd0dcfc109fae520e7f601ab
sukh <sukh@gmail.om.com>
2023-07-16 21:43:54 +0530

    commie

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,6 @@
 def call(){
     withDockerRegistry(credentialsId: 'dockerhub', url: 'https://index.docker.io/v1/') {
-        sh 'docker push ' + "${IMAGE_NAME}" + ":" + 'latest'
+        sh "docker push sukhpreet2002/spring-app:latest"
     }
 
 }
