e8028563020a3b4c25ba1b7e67c5c9f5fa86ce6e
sukh <sukh@gmail.om.com>
2023-07-16 22:04:42 +0530

    commie

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -1,4 +1,4 @@
 def call(){
-    sh "docker build -t sukhpreet2002/spring-app" + "${BUILD_NUMBER}" + "./GthubActions/"
+    sh "docker build -t sukhpreet2002/spring-app" + ":" +"${BUILD_NUMBER}" + "./GthubActions/"
     sh "docker build -t sukhpreet2002/spring-app ./GthubActions/"
 }
